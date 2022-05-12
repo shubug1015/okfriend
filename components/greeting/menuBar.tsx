c15f6d456906241e8f4ff61f86274b870280f52f
shubug1015 <shubug@naver.com>
2022-05-12 15:21:56 +0900

    error fixed

--- components/greeting/menuBar.tsx
+++ components/greeting/menuBar.tsx
@@ -1,3 +1,4 @@
+import { useLocale } from '@libs/client/useLocale';
 import { cls } from '@libs/client/utils';
 import Link from 'next/link';
 
@@ -6,13 +7,14 @@ interface IProps {
 }
 
 export default function MenuBar({ pageName }: IProps) {
+  const { text } = useLocale();
   return (
     <div className='flex items-center justify-center border-b border-[#ebebeb] bg-white text-[1.375rem] text-[#9e9e9e] md:border-transparent md:text-base'>
       <Link href='/course-introduction/greeting'>
         <a>
           <div
             className={cls(
-              pageName === '인사말'
+              pageName === text.greeting['4']
                 ? 'border-[#2fb6bc]  text-[#2fb6bc]'
                 : 'border-transparent',
               'flex h-[4.188rem] w-[12.5rem] items-center justify-center border-b-4 font-bold md:h-[3.125rem] md:w-[5.75rem] md:border-b-2'
@@ -27,7 +29,7 @@ export default function MenuBar({ pageName }: IProps) {
         <a>
           <div
             className={cls(
-              pageName === '개요'
+              pageName === text.summary['5']
                 ? 'border-[#2fb6bc] font-bold text-[#2fb6bc]'
                 : 'border-transparent',
               'flex h-[4.188rem] w-[12.5rem] items-center justify-center border-b-4 font-bold md:h-[3.125rem] md:w-[5.75rem] md:border-b-2'
@@ -42,7 +44,7 @@ export default function MenuBar({ pageName }: IProps) {
         <a>
           <div
             className={cls(
-              pageName === '연수 편성표'
+              pageName === text.schedule['6']
                 ? 'border-[#2fb6bc] font-bold text-[#2fb6bc]'
                 : 'border-transparent',
               'flex h-[4.188rem] w-[12.5rem] items-center justify-center border-b-4 font-bold md:h-[3.125rem] md:w-[5.75rem] md:border-b-2'
