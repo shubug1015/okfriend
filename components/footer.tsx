38f3895dc7264fa8b1f879e4990048e5f27a5a81
kyoungsic-dev <sick93s@naver.com>
2022-06-16 15:54:04 +0900

    백호 SVG 수정

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 export default function Footer() {
   const { locale, text } = useLocale();
   return (
-    <footer className='w-screen bg-[#ffffff] pt-14 pb-8 md:pt-10 md:pb-14'>
+    <footer className='w-screen bg-[#01111e] pt-14 pb-8 md:pt-10 md:pb-14'>
       <div className='mx-auto flex max-w-[1400px] md:flex-col md:items-center'>
         <div className='w-1/3 md:flex md:w-full md:justify-center'>
           <FooterLogo />
@@ -26,7 +26,7 @@ export default function Footer() {
         </div>
 
         <div className='-mt-1.5 w-1/3 md:mt-0 md:flex md:w-full md:justify-center'>
-          <div className='text-[#333333] md:mt-3.5 md:text-center md:text-[0.688rem]'>
+          <div className='text-[#d6d6d6] md:mt-3.5 md:text-center md:text-[0.688rem]'>
             {text.footer['1']}
             <br />
             82-2-2137-2402
@@ -34,9 +34,7 @@ export default function Footer() {
             okfteencamp@gmail.com
           </div>
 
-          <div className='mt-10 text-sm text-[#333333] md:hidden'>
-            {text.footer['2']}
-          </div>
+          <div className='mt-10 text-sm text-[#d6d6d6] md:hidden'>{text.footer['2']}</div>
         </div>
 
         <div className='flex w-1/3 flex-col items-end justify-between md:w-full md:items-center'>
@@ -50,21 +48,14 @@ export default function Footer() {
             className={cls(
               clsFilter(locale, 'space-x-4', 'space-x-2', 'space-x-4'),
               'mt-8 flex items-center md:mt-5 md:space-x-2.5 md:text-[0.8rem]'
-            )}
-          >
+            )}>
             <Link href='/service-policy'>
               <a>
                 <div
                   className={cls(
-                    clsFilter(
-                      locale,
-                      'text-sm',
-                      'text-xs',
-                      'text-xs md:text-[0.5rem]'
-                    ),
+                    clsFilter(locale, 'text-sm', 'text-xs', 'text-xs md:text-[0.5rem]'),
                     'font-notoSans text-[#d6d6d6]'
-                  )}
-                >
+                  )}>
                   {text.footer['3']}
                 </div>
               </a>
@@ -75,15 +66,9 @@ export default function Footer() {
               <a>
                 <div
                   className={cls(
-                    clsFilter(
-                      locale,
-                      'text-sm',
-                      'text-xs',
-                      'text-xs md:text-[0.5rem]'
-                    ),
+                    clsFilter(locale, 'text-sm', 'text-xs', 'text-xs md:text-[0.5rem]'),
                     'font-notoSans text-[#d6d6d6]'
-                  )}
-                >
+                  )}>
                   {text.footer['4']}
                 </div>
               </a>
@@ -94,15 +79,9 @@ export default function Footer() {
               <a>
                 <div
                   className={cls(
-                    clsFilter(
-                      locale,
-                      'text-sm',
-                      'text-xs',
-                      'text-xs md:text-[0.5rem]'
-                    ),
+                    clsFilter(locale, 'text-sm', 'text-xs', 'text-xs md:text-[0.5rem]'),
                     'font-notoSans text-[#d6d6d6]'
-                  )}
-                >
+                  )}>
                   {text.footer['5']}
                 </div>
               </a>
