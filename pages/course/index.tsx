dd2a5b548b2f757fb5f1e053ff7fea27e7f4a2fd
Viaduct <alien@reeddst.com>
2022-06-30 19:24:33 +0900

    Block user when they're trying to watch videos when they should not.

--- pages/course/index.tsx
+++ pages/course/index.tsx
@@ -11,10 +11,90 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import { useUser } from '@libs/client/useUser';
+import { useRouter } from 'next/router'
+
+const useRestrictCourseEntryByStage = (profile: any) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const impl = async () => {
+      let errorMessage: string | undefined;
+
+      if (profile == null) {
+        errorMessage = '강좌를 시청하기 위해서는 로그인이 필요합니다.';
+      } else {
+        const now = new Date();
+        switch (profile.stage) {
+          case 1: {
+            const julyBegin = new Date('2022-07-01');
+            const julyEnd = new Date('2022-08-01');
+
+            const isNowInJuly = julyBegin.valueOf() <= now.valueOf() && now.valueOf() < julyEnd.valueOf();
+
+            if (!isNowInJuly) {
+              errorMessage = '강좌를 시청 가능한 날짜가 아닙니다.(1기는 7월에 시청가능)';
+            }
+
+            break;
+          }
+
+          case 2: {
+            const augustBegin = new Date('2022-08-01');
+            const augustEnd = new Date('2022-09-01');
+
+            const isNowInAugust = augustBegin.valueOf() <= now.valueOf() && now.valueOf () < augustEnd.valueOf();
+
+            if (!isNowInAugust) {
+              errorMessage = '강좌를 시청 가능한 날짜가 아닙니다.(2기는 8월에 시청가능)';
+            }
+
+            break;
+          }
+
+          case 3: {
+            const septemberBegin = new Date('2022-09-01');
+            const septemberEnd = new Date('2022-10-01');
+
+            const isNowInSeptember = septemberBegin.valueOf() <= now.valueOf() && now.valueOf() < septemberEnd.valueOf();
+
+            if (!isNowInSeptember) {
+              errorMessage = '강좌를 시청 가능한 날짜가 아닙니다.(3기는 9월에 시청가능)';
+            }
+
+            break;
+          }
+
+          case 4: {
+            const octoberBegin = new Date('2022-10-01');
+            const octoberEnd = new Date('2022-11-01');
+
+            const isNowInOctober = octoberBegin.valueOf() <= now.valueOf() && now.valueOf() < octoberEnd.valueOf();
+
+            if (!isNowInOctober) {
+              errorMessage = '강좌를 시청 가능한 날짜가 아닙니다.(4기는 10월에 시청가능)';
+            }
+
+            break;
+          }
+        }
+      }
+
+
+      if (errorMessage != null) {
+        alert(errorMessage);
+        await router.push('/');
+      }
+    };
+
+    impl();
+  }, []);
+};
 
 const Course: NextPage = () => {
   const { locale, text } = useLocale();
   const { profile } = useUser({ isPrivate: true });
+
+  useRestrictCourseEntryByStage(profile);
 
   useEffect(() => {
     AOS.init({ once: true });
