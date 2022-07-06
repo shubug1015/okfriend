c76909e6fa54b2c0214bf0a66ab124842681ea8e
김린회 <flsghl@naver.com>
2022-07-06 16:10:50 +0900

    add meta pixel add ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,6 +28,41 @@ class CustomDocument extends Document<IProps> {
             href='https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap'
             rel='stylesheet'
           />
+          {/* GA Code */}
+          <script
+            async
+            src='https://www.googletagmanager.com/gtag/js?id=AW-10924881447'
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  
+                  gtag('config', 'AW-10924881447');
+              `,
+            }}
+          />
+          {/* Meta Pixel Code */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=‘2.0’;
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,‘script’,
+      ‘https://connect.facebook.net/en_US/fbevents.js’);
+      fbq(‘init’, ‘605386027285587’);
+      fbq(‘track’, ‘PageView’);`,
+            }}/>
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<img height=“1” width=“1" style=“display:none”
+src=“https://www.facebook.com/tr?id=605386027285587&ev=PageView&noscript=1”
+  />`,
+          }}/>
         </Head>
         <body>
           <Main />
